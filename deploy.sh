b35647f26fd2249c6def333724640feca29d7771
Matsushin <matsu1983@gmail.com>
2019-04-02 16:01:13 +0900

    設定変更

--- deploy.sh
+++ deploy.sh
@@ -110,7 +110,7 @@ make_task_def(){
 		}
 	]'
 	
-	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
+	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1 $AWS_ACCOUNT_ID $CIRCLE_SHA1 $AWS_ACCOUNT_ID $CIRCLE_SHA1 $AWS_ACCOUNT_ID $CIRCLE_SHA1)
 }
 
 push_ecr_image(){
